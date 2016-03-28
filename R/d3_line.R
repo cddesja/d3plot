09e095a6166f82736a79ccfae35ec52b43bc8a92
Christopher David Desjardins <cddesjardins@gmail.com>
2016-03-28 16:09:19 -0500

    added color and group to line plot

--- R/d3_line.R
+++ R/d3_line.R
@@ -26,18 +26,36 @@ function type (d){
   d.y = +d.y;
 return d;
 };')
+  if(grepl("color", data)){
+  tmpfile <- paste0(tmpfile, 'nestedData = d3.nest().key(function(d) { return d.color; }).entries(dataset);')
+  }
+  if(grepl("group", data)){
+    tmpfile <- paste0(tmpfile, 'nestedData = d3.nest().key(function(d) { return d.group; }).entries(dataset);')
+  }
 
 tmpfile <- paste0(tmpfile, setup_canvas(arguments),' function render(data) {', setup_axes(),'
 var line = d3.svg.line()
     .x(function(d) { return xScale(d.x); })
     .y(function(d) { return yScale(d.y); });
-svg.append("path")
+')
+if(grepl("color", data) | grepl("group", data)){
+  tmpfile <- paste0(tmpfile, 'var groups = svg.selectAll(".group")
+      .data(nestedData, function(d) { return d.key; })
+                    .enter().append("g");
+                    groups.append("path")
+                    .attr("class", "line")
+                    .attr("d", function(d) { return line(d.values); })
+                    .attr("stroke-width", "1px")
+                    .attr("fill", "none")
+                    .style("stroke", function(d) { return color(d.key); });')
+} else tmpfile <- paste0(tmpfile,'svg.append("path")
       .datum(data)
        .attr("class", "line")
         .attr("fill", "none")
         .attr("stroke", "black")
         .attr("stroke-width", "1px")
-       .attr("d", line);
+       .attr("d", line);')
+tmpfile <- paste0(tmpfile,'
 };
 render(dataset);
 ')
