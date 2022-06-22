1385238a7ed78899a9b6eace7000b8ddec7248e9
Parker Lambert <89795263+plambert777@users.noreply.github.com>
2022-06-22 09:13:28 -0400

    updated var to const and addressable h/w Values
    now can adjust size by changing the heightValue and widthValue

--- R/setup_canvas.R
+++ R/setup_canvas.R
@@ -1,40 +1,43 @@
 setup_canvas <- function(...){
-  setup <- paste0('var margin = {top: 50, right: 50, bottom: 30, left: 50},
-  width = 600 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom
+  setup <- paste0('
+  const heightValue = 600;
+  const widthValue = 500;
+  const margin = {top: 50, right: 50, bottom: 60, left: 50},
+  width = heightValue - margin.left - margin.right,
+  height = widthValue - margin.top - margin.bottom
   yTicks = 5;
 
-  var color = d3.scaleOrdinal(d3.schemeCategory10);
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  var xScale = d3.scaleLinear()
+  const xScale = d3.scaleLinear()
   .range([0, width]);
 
-  var yScale = d3.scaleLinear()
+  const yScale = d3.scaleLinear()
   .range([height, 0]);
 
-  var xAxisBot = d3.axisBottom()
+  const xAxisBot = d3.axisBottom()
   .scale(xScale)
   .ticks(yTicks)
   .tickSizeOuter(0);
 
-  var xAxisTop = d3.axisTop()
+  const xAxisTop = d3.axisTop()
   .scale(xScale)
   .ticks(yTicks)
   .tickFormat("")
   .tickSizeOuter(0);
 
-  var yAxisLeft = d3.axisLeft()
+  const yAxisLeft = d3.axisLeft()
   .scale(yScale)
   .ticks(yTicks)
   .tickSizeOuter(0);
 
-  var yAxisRight = d3.axisRight()
+  const yAxisRight = d3.axisRight()
   .scale(yScale)
   .ticks(yTicks)
   .tickFormat("")
   .tickSizeOuter(0);
 
-  var svg = d3.select("body").append("svg")
+  const svg = d3.select("body").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
