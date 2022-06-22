a74dc23e5640099fdfec96f8d5c63cef40c2cb90
Parker Lambert <89795263+plambert777@users.noreply.github.com>
2022-06-22 09:11:09 -0400

    Now has addressable title and lables
    Title = "your Title", xTitle = "your xTitle", yTitle = "your yTitle"

--- R/setup_axes.R
+++ R/setup_axes.R
@@ -1,6 +1,6 @@
-setup_axes <- function(...){
+setup_axes <- function(arguments){
   setup <- paste0(setup_extents(),'
-svg.append("g")
+                      svg.append("g")
                        .attr("class", "x axis")
                        .attr("transform", "translate(0," + height + ")")
                        .call(xAxisBot)
@@ -10,12 +10,12 @@ svg.append("g")
                        .attr("y", 29)
                        .style("text-anchor", "middle")
                        .text("");
-
-                       svg.append("g")
+                       
+                      svg.append("g")
                        .attr("class", "x axis")
                        .call(xAxisTop);
-
-                       svg.append("g")
+                       
+                      svg.append("g")
                        .attr("class", "y axis")
                        .call(yAxisLeft)
                        .append("text")
@@ -26,11 +26,47 @@ svg.append("g")
                        .attr("dy", ".71em")
                        .style("text-anchor", "middle")
                        .text("");
-
-                       svg.append("g")
+                       
+                      svg.append("g")
                        .attr("class", "y axis")
                        .attr("transform", "translate(" + width + ", 0)")
                        .call(yAxisRight);
+                      
+                      // add chart title label 
+                      svg.append("text")
+                       .attr("x", width/2) //could also be written (widthValue + margin.left - margin.right)/2)
+                       .attr("y", -20)
+                       .attr("text-anchor", "middle")
+                       .style("font-size", "20px")')
+                      if(any(names(arguments) == "Title")){
+                        setup <- paste0(setup, '.text("',eval(arguments$Title),'");')
+                      } else  setup <- paste0(setup,'.text("");')
+                      setup <- paste0(setup,'
+                      
+                      // add y axis lable
+                      svg.append("text")
+                      .attr("transform", "rotate(-90)")
+                      .attr("y", 0 - margin.left)
+                      .attr("x",0 - (height / 2))
+                      .attr("dy", "1em")
+                      .style("text-anchor", "middle")')
+                      if(any(names(arguments) == "yTitle")){
+                        setup <- paste0(setup, '
+                                .text("',eval(arguments$yTitle),'");')
+                      } else  setup <- paste0(setup,'
+                                .text("");')
+                      setup <- paste0(setup,'  
+                      
+                      // add x axis lable
+                      svg.append("text")             
+                      .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top - 10) + ")")
+                      .style("text-anchor", "middle")')
+                      if(any(names(arguments) == "xTitle")){
+                        setup <- paste0(setup, '
+                                      .text("',eval(arguments$xTitle),'");')
+                      } else  setup <- paste0(setup,'
+                                      .text("");')
+                      setup <- paste0(setup,'
                        ')
   return(setup)
 }
