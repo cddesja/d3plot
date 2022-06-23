ea3c24ab21f78af4e2435f28d7946c699b09d987
Parker Lambert <89795263+plambert777@users.noreply.github.com>
2022-06-23 10:05:33 -0400

    var to const

--- R/setup_extents.R
+++ R/setup_extents.R
@@ -1,13 +1,12 @@
 setup_extents <- function(){
-setup <- paste0('
+  setup <- paste0('
   // get extents and range
-  var xExtent = d3.extent(data, function(d) { return d.x; }),
+  const xExtent = d3.extent(data, function(d) { return d.x; }),
       xRange = xExtent[1] - xExtent[0],
       yExtent = d3.extent(data, function(d) { return d.y; }),
       yRange = yExtent[1] - yExtent[0];
-
   // set domain to be extent +- 5%
   xScale.domain([xExtent[0] - (xRange * .05), xExtent[1] + (xRange * .05)]);
   yScale.domain([yExtent[0] - (yRange * .05), yExtent[1] + (yRange * .05)]);')
-return(setup)
+  return(setup)
 }
